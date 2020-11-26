14865ab238bf170404c35acc9951d859d5ddce1b
DESKTOP-IBPR654\User <tobias@fraunberger.co.at>
2020-11-26 08:32:53 +0100

    Projektdateien hinzufügen.

---
+++ Chinook.ConApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chinook.ConApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Chinook-Marketing!");
+
+
+			var trackStatistic = Report.MarketingReports.GetTrackStatistics();
+            Console.WriteLine("Track Statistic");
+            Console.WriteLine($"Max Time: {trackStatistic.MaxTime}");
+            Console.WriteLine($"Min Time: {trackStatistic.MinTime}");
+            Console.WriteLine($"Average Time: {trackStatistic.AverageTime}");
+
+
+			var albumStatistic = Report.MarketingReports.GetAlbumStatistics();
+			Console.WriteLine("\nAlbum Statistic");
+			Console.WriteLine($"Max Time: {albumStatistic.MaxTime}");
+			Console.WriteLine($"Min Time: {albumStatistic.MinTime}");
+			Console.WriteLine($"Average Time: {albumStatistic.AverageTime}");
+
+
+		}
+	}
+}
---
+++ Chinook.Report/MarketingReports.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Chinook.Report.Marketing.Models;
+using Chinook.Contracts.Report.Marketing;
+
+namespace Chinook.Report
+{
+	public class MarketingReports
+	{
+		public static IEnumerable<Contracts.Report.Marketing.IArtistStatistic> GetArtistStatistics()
+		{
+			var albums = Logic.Factory.GetAllAlbums();
+			var artists = Logic.Factory.GetAllArtists();
+			var genres = Logic.Factory.GetAllGenres();
+
+			// Abfrage 
+			var result = default(IEnumerable<Contracts.Report.Marketing.IArtistStatistic>);
+
+			return result;
+		}
+
+		public static ITrackStatistic GetTrackStatistics()
+        {
+			var tracks = Logic.Factory.GetAllTracks();
+
+			TrackStatistic trackStatistic = new TrackStatistic();
+
+			trackStatistic.MaxTime = tracks.Max(x=> x.Milliseconds/1000);
+
+			trackStatistic.MinTime = tracks.Min(x => x.Milliseconds / 1000);
+
+			trackStatistic.AverageTime = tracks.Average(x => x.Milliseconds / 1000);
+
+			return trackStatistic;
+		}
+
+		public static IAlbumStatistic GetAlbumStatistics()
+        {
+			var albums = Logic.Factory.GetAllAlbums();
+			var tracks = Logic.Factory.GetAllTracks();
+
+			AlbumStatistic albumStatistic = new AlbumStatistic();
+
+			var AlbumTime = from a in albums
+								 join t in tracks on a.Id equals t.AlbumId
+								 group t by new { a.Id } into g
+								 select new
+								 {
+									 AlbumTime = g.Sum(x => x.Milliseconds)
+								 };
+
+			albumStatistic.MaxTime = AlbumTime.Max(x => x.AlbumTime);
+			albumStatistic.MinTime = AlbumTime.Min(x => x.AlbumTime);
+			albumStatistic.AverageTime = AlbumTime.Average(x => x.AlbumTime);
+
+			return albumStatistic;
+
+		}
+	}
+}
